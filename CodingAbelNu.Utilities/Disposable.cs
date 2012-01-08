3d8cd66c88a5fc88924857f0433d09cb9f5059cb
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-01-08 20:33:52 +0000

    - Finalized Disposable class - Added LogWriterFromDisposable sample.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@44 1977e728-b283-c84d-8f65-b9948314f50f

--- CodingAbelNu.Utilities/Disposable.cs
+++ CodingAbelNu.Utilities/Disposable.cs
@@ -5,40 +5,40 @@ using System.Text;
 
 namespace CodingAbelNu.Utilities
 {
+/// <summary>
+/// A general implementation of the disposable pattern. See http://coding.abel.nu/2012/01/disposable
+/// </summary>
+public abstract class Disposable : IDisposable
+{
     /// <summary>
-    /// A general implementation of the disposable pattern. See http://coding.abel.nu/2012/01/disposable
+    /// Implementation of IDisposable.Dispose method.
     /// </summary>
-    public abstract class Disposable : IDisposable
+    public void Dispose()
     {
-        /// <summary>
-        /// Implementation of IDisposable.Dispose method.
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Is this instance disposed?
-        /// </summary>
-        protected bool IsDisposed { get; private set; }
-
-        /// <summary>
-        /// Dispose worker method. See http://coding.abel.nu/2012/01/disposable
-        /// </summary>
-        /// <param name="disposing">Are we disposing? Otherwise we're finalizing.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            IsDisposed = true;
-        }
-
-        /// <summary>
-        /// Finalizer.
-        /// </summary>
-        ~Disposable()
-        {
-            Dispose(false);
-        }
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
+
+    /// <summary>
+    /// Is this instance disposed?
+    /// </summary>
+    protected bool Disposed { get; private set; }
+
+    /// <summary>
+    /// Dispose worker method. See http://coding.abel.nu/2012/01/disposable
+    /// </summary>
+    /// <param name="disposing">Are we disposing? Otherwise we're finalizing.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        Disposed = true;
+    }
+
+    /// <summary>
+    /// Finalizer.
+    /// </summary>
+    ~Disposable()
+    {
+        Dispose(false);
+    }
+}
 }
---
+++ TestLib/LogWriterFromDisposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodingAbelNu.Utilities;
+using System.IO;
+
+namespace TestLib
+{
+public class LogWriterFromDisposable : Disposable
+{
+    private StreamWriter m_Stream;
+
+    public LogWriterFromDisposable(string logFile)
+    {
+        m_Stream = new StreamWriter(logFile, true);
+        m_Stream.WriteLine("Starting logging at {0}", DateTime.Now);
+    }
+
+    public void WriteLine(string message)
+    {
+        m_Stream.WriteLine(message);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!Disposed && disposing)
+        {
+            m_Stream.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+}
+}
