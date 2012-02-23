9416bfd3a641c6e60cabedf9c58f75a88681638c
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-02-23 15:38:19 +0000

    - Added a CarsContext ctor with connection string as parameter to enable use from linqpad.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@58 1977e728-b283-c84d-8f65-b9948314f50f

--- TestLib/Entities/CarsContext.cs
+++ TestLib/Entities/CarsContext.cs
@@ -7,14 +7,20 @@ using CodingAbelNu.Utilities.EntityFramework;
 
 namespace TestLib.Entities
 {
-public class CarsContext : DbContext
-{
-static CarsContext()
-{ 
-    Database.SetInitializer(new ValidateDatabase<CarsContext>());
-}
+    public class CarsContext : DbContext
+    {
+        public CarsContext(string connectionString)
+            : base(connectionString)
+        {}
 
-    public DbSet<Car> Cars { get; set; }
-    public DbSet<Brand> Brands { get; set; }
-}
+        public CarsContext() { }
+
+        static CarsContext()
+        {
+            Database.SetInitializer(new ValidateDatabase<CarsContext>());
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
 }
