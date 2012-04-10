557c9a461192fd9c3bea5e41a31cf84d7cff8908
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-04-10 12:31:30 +0000

    - Added Dynamic Overload Resolution sample.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@68 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/DynamicOverloads.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace TestLib
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+    public class Elephant : Animal { }
+
+public static class AnimalTransport
+{
+    public static void LoadAnimal(Animal animal)
+    {
+        Debug.WriteLine("Putting " + animal.Name + " in a cage.");
+    }
+
+    public static void LoadAnimal(Elephant elephant)
+    {
+        Debug.WriteLine("Loading " + elephant.Name + " on a trailer.");
+    }
+}
+
+public interface ITransportationService
+{
+    void TransportAnimal(Animal a);
+}
+
+public class SimpleTransportation : ITransportationService
+{
+    public void TransportAnimal(Animal a)
+    {
+        AnimalTransport.LoadAnimal(a);
+    }
+}
+
+public class FlexibleTransportation : ITransportationService
+{
+    public void TransportAnimal(Animal a)
+    {
+        AnimalTransport.LoadAnimal((dynamic)a);
+    }
+}
+}
---
+++ TestLibTests/DynamicOverloadsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TestLib;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class DynamicOverloadsTests
+    {
+        [TestMethod]
+        public void TestSimple()
+        {
+            Elephant elephant = new Elephant() { Name = "Hanibal" };
+            
+            Debug.WriteLine("Transporting an elephant with SimpleTransportation...");
+            new SimpleTransportation().TransportAnimal(elephant);
+
+            Debug.WriteLine("Transporting an elephant with FlexibleTransportation...");
+            new FlexibleTransportation().TransportAnimal(elephant);
+        }
+    }
+}
