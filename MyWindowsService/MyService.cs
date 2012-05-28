94677ff706b0588f2c3dae1ba85e14bbd97d8362
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-05-28 17:27:51 +0000

    - Adding code for Debugging Windows Service post.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@73 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ MyWindowsService/MyService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+
+namespace MyWindowsService
+{
+    public partial class MyService : ServiceBase
+    {
+        StreamWriter log = new StreamWriter("c:\\temp\\myservice.log", true);
+
+        public MyService()
+        {
+            InitializeComponent();
+
+            Disposed += Dispose;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.WriteLine("{0}: MyService Started!", DateTime.Now);
+            log.Flush();
+        }
+
+        protected override void OnStop()
+        {
+            log.WriteLine("{0}: MyService Stopped!", DateTime.Now);
+            log.Flush();
+        }
+
+        void Dispose(object sender, EventArgs e)
+        {
+            log.Dispose();
+        }
+    }
+}
---
+++ MyWindowsService/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Reflection;
+using System.Threading;
+
+namespace MyWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new MyService() 
+			};
+            if (Environment.UserInteractive)
+            {
+                RunInteractive(ServicesToRun);
+            }
+            else
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        static void RunInteractive(ServiceBase[] servicesToRun)
+        {
+            Console.WriteLine("Services running in interactive mode.");
+            Console.WriteLine();
+
+            MethodInfo onStartMethod = typeof(ServiceBase).GetMethod("OnStart",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Starting {0}...", service.ServiceName);
+                onStartMethod.Invoke(service, new object[] { new string[] { } });
+                Console.Write("Started");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(
+                "Press any key to stop the services and end the process...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            MethodInfo onStopMethod = typeof(ServiceBase).GetMethod("OnStop",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ServiceBase service in servicesToRun)
+            {
+                Console.Write("Stopping {0}...", service.ServiceName);
+                onStopMethod.Invoke(service, null);
+                Console.WriteLine("Stopped");
+            }
+
+            Console.WriteLine("All services stopped.");
+            Thread.Sleep(1000);
+        }
+    }
+}
