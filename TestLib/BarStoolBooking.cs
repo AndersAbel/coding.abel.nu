b0ea31e35bda7d845f0fcd5a75cc3b39e67c5206
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-05-28 18:43:57 +0000

    - Added files for Bitten by yield return post.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@74 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/BarStoolBooking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib
+{
+    public class BarStoolBooking
+    {
+        static int nextBookingNumber = 1;
+
+        public int BookingNumber { get; private set; }
+        public string Name { get; set; }
+        public int BarStoolNumber { get; set; }
+        public DateTime Date { get; set; }
+
+        public BarStoolBooking Submit()
+        {
+            BookingNumber = nextBookingNumber++;
+            return this;
+        }
+
+        public static IEnumerable<BarStoolBooking> SubmitSeries(BarStoolBooking bookingInfo,
+            DateTime firstDate, DateTime lastDate)
+        {
+            for (DateTime date = firstDate; date <= lastDate; date = date.AddDays(1))
+            {
+                bookingInfo.Date = date;
+                yield return bookingInfo.Submit();
+            }
+        }
+    }
+}
---
+++ TestLibTests/BarStoolBookingTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib;
+using System.Diagnostics;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class BarStoolBookingTest
+    {
+        private static void PrintHeader(BarStoolBooking booking)
+        {
+            Debug.WriteLine("{0} is booked for {1} (ref no {2}) the following dates:",
+                booking.BarStoolNumber, booking.Name, booking.BookingNumber);
+        }
+
+        private static void PrintFooter(BarStoolBooking booking)
+        {
+            Debug.WriteLine("End of dates (ref no {0})", booking.BookingNumber);
+        }
+
+        [TestMethod]
+        public void TestSeriesBooking()
+        {
+            BarStoolBooking baseBooking = new BarStoolBooking()
+            {
+                Name = "Arthur Dent",
+                BarStoolNumber = 42
+            };
+
+var bookings = BarStoolBooking.SubmitSeries(baseBooking,
+    DateTime.Now.Date, DateTime.Now.Date.AddDays(1));
+
+PrintHeader(bookings.First());
+
+foreach (var b in bookings)
+{
+    Debug.WriteLine(b.Date.ToShortDateString());
+}
+
+PrintFooter(bookings.First());
+        }
+    }
+}
