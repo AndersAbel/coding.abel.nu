0dbc47202f395c5a223c33e8d8fff376769f8566
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-06-10 20:58:15 +0000

    - Added navigation property post code.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@78 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/PersonInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public string BornIn { get; set; }
+        public string LivesIn { get; set; }
+        public string Gender { get; set; }
+        public int CarsOwnedCount { get; set; }
+
+        public static PersonInfo GetWithJoin(int personId)
+        {
+            using (DBContext ctx = new DBContext())
+            {
+                return (
+from p in ctx.Persons
+where p.ID == personId
+join bornIn in ctx.Cities
+on p.BornIn equals bornIn.CityID
+join livesIn in ctx.Cities
+on p.LivesIn equals livesIn.CityID
+join s in ctx.Sexes
+on p.SexID equals s.ID
+select new PersonInfo
+{
+    Name = p.FirstName + " " + p.LastName,
+    BornIn = bornIn.Name,
+    LivesIn = livesIn.Name,
+    Gender = s.Name,
+    CarsOwnedCount = ctx.Cars.Where(c => c.OwnerID == p.ID).Count()
+}
+                        ).Single();
+            }
+        }
+
+        public static PersonInfo GetWithNavProperties(int personId)
+        {
+            using (DBContext ctx = new DBContext())
+            { 
+                return (
+from p in ctx.Persons
+where p.ID == personId
+select new PersonInfo
+{
+    Name = p.FirstName + " " + p.LastName,
+    BornIn = p.BornInCity.Name,
+    LivesIn = p.LivesInCity.Name,
+    Gender = p.Sex.Name,
+    CarsOwnedCount = p.Cars.Count(),
+}
+                        ).Single();
+            }
+        }
+    }
+}
---
+++ TestLibTests/NavigationPropertiesTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class NavigationPropertiesTest
+    {
+        private void CheckValues(PersonInfo pi)
+        {
+            Assert.AreEqual("Nisse Karlsson", pi.Name);
+            Assert.AreEqual(7, pi.CarsOwnedCount);
+        }
+
+        [TestMethod]
+        public void TestJoin()
+        {
+            CheckValues(PersonInfo.GetWithJoin(3));
+        }
+
+        [TestMethod]
+        public void TestNav()
+        {
+            CheckValues(PersonInfo.GetWithNavProperties(3));
+        }
+
+        [TestMethod]
+        public void GetSimple()
+        {
+            using (DBContext ctx = new DBContext())
+            {
+                var q1 =
+from p in ctx.Persons
+join c in ctx.Cities
+on p.BornIn equals c.CityID
+select new
+{
+    p.FirstName,
+    c.Name
+};
+
+                var q2 =
+from p in ctx.Persons
+select new
+{
+    p.FirstName,
+    p.BornInCity.Name
+};
+
+            }
+        }
+    }
+}
