4490688e1e221142d458ebea20fe37bd52d67697
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-07-05 21:49:25 +0000

    - Added ReadModel examples.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@80 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/Entities/ReadModel/CarReadModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLib.Entities;
+
+namespace TestLib.Entities.ReadModel
+{
+public class CarReadModel
+{
+    public Car Car { get; set; }
+
+    public string BrandName { get; set; }
+}
+
+}
---
+++ TestLib/Entities/ReadModel/CarReadModelRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib.Entities.ReadModel
+{
+    public class CarReadModelRepository
+    {
+        public CarReadModel GetById(int id)
+        {
+            using (CarsContext ctx = new CarsContext())
+            {
+                return (
+from c in ctx.Cars
+where c.CarId == id
+select new CarReadModel
+{
+    Car = c,
+    BrandName = c.Brand.Name
+}
+                    ).Single();
+            }
+        }
+    }
+}
