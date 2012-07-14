ceae41604d3000a0cde614c51770097672ebddc3
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-07-14 14:04:39 +0000

    - Added samples for ReadModel select extension methods.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@81 1977e728-b283-c84d-8f65-b9948314f50f

--- TestLib/Entities/ReadModel/CarReadModelRepository.cs
+++ TestLib/Entities/ReadModel/CarReadModelRepository.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace TestLib.Entities.ReadModel
 {
-    public class CarReadModelRepository
+    public static class CarReadModelRepository
     {
-        public CarReadModel GetById(int id)
+        public static CarReadModel GetById(int id)
         {
             using (CarsContext ctx = new CarsContext())
             {
@@ -20,6 +20,51 @@ select new CarReadModel
     BrandName = c.Brand.Name
 }
                     ).Single();
+            }
+        }
+
+public static IQueryable<CarReadModel> SelectCarReadModel(this IQueryable<Car> query)
+{
+    return query.Select(c => new CarReadModel
+    {
+        Car = c,
+        BrandName = c.Brand.Name
+    });
+}
+
+        public static CarReadModel SearchByBrand(string brandFilter)
+        {
+            using (CarsContext ctx = new CarsContext())
+            {
+                return (
+from c in ctx.Cars.SelectCarReadModel()
+where c.BrandName.StartsWith(brandFilter)
+select c
+                ).Single();
+            }   
+        }
+
+        public static CarReadModel GetById2(int id)
+        {
+            using( CarsContext ctx = new CarsContext())
+            {
+                return
+(from c in ctx.Cars
+ where c.CarId == id
+ select c).SelectCarReadModel()
+                .Single();
+            }
+        }
+        
+        public static CarReadModel GetById3(int id)
+        {
+            using (CarsContext ctx = new CarsContext())
+            {
+                return (
+from c in ctx.Cars.SelectCarReadModel()
+where c.Car.CarId == id
+select c
+                ).Single();
             }
         }
     }
---
+++ TestLibTests/ReadModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib.Entities.ReadModel;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class ReadModel
+    {
+        [TestMethod]
+        public void RunGetById()
+        {
+            CarReadModelRepository.GetById(4);
+        }
+
+        [TestMethod]
+        public void RunGetById2()
+        {
+            CarReadModelRepository.GetById2(4);
+        }
+
+        [TestMethod]
+        public void RunGetById3()
+        {
+            CarReadModelRepository.GetById3(4);
+        }
+    }
+}
