b7fbf1bc7822fee7f8b8f3d93b107c4d89393de6
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-10-15 18:34:53 +0000

    - Testing different EF code first mappings.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@91 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/UnitOfWork/SharedQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLib.Entities;
+
+namespace TestLib.UnitOfWork
+{
+    public static class SharedQueries
+    {
+public static Entities.Car GetCar(int id)
+{
+    using (var uow = new UnitOfWorkScope<CarsContext>(UnitOfWorkScopePurpose.Reading))
+    {
+        return uow.DbContext.Cars.Single(c => c.CarId == id);
+    }
+}
+    }
+}
