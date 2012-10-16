ccdbb5a61786f9849cf5ccaa3b9922a97eb48cdd
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-10-16 12:06:51 +0000

    - Added example of subscribing to an event with a lambda that captures a variable.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@93 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLib/LambdaEventSubscribe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace TestLib
+{
+    public static class LambdaEventSubscribe
+    {
+public static void Run(Stack<int> result)
+{
+    ScheduleAdd(result, number: 1, timeoutms: 2);
+    ScheduleAdd(result, number: 2, timeoutms: 1);
+
+    System.Threading.Thread.Sleep(20);
+}
+
+private static void ScheduleAdd(Stack<int> stack, int number, int timeoutms)
+{
+    var t = new Timer(timeoutms) { AutoReset = false };
+
+    t.Elapsed += (object sender, ElapsedEventArgs eventArgs) => stack.Push(number);
+    t.Enabled = true;
+}
+    }
+}
---
+++ TestLibTests/LambdaEventSubscribeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class LambdaEventSubscribeTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var result = new Stack<int>();
+
+            LambdaEventSubscribe.Run(result);
+
+            Assert.AreEqual(2, result.Count, "Count");
+            Assert.AreEqual(1, result.Pop());
+            Assert.AreEqual(2, result.Pop());
+        }
+    }
+}
