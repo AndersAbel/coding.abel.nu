a19c1ba0be6b971e1d5336caa1dfc5848b7e573a
anders <anders@1977e728-b283-c84d-8f65-b9948314f50f>
2012-12-13 21:19:24 +0000

    - Removed MvcApplication.csproj.user file from source control - should never have been checked in. - Added test code for EfSavingChanges post.
    git-svn-id: svn+ssh://hemma.abel.nu/BlogCode/trunk@94 1977e728-b283-c84d-8f65-b9948314f50f

---
+++ TestLibTests/EfSavingChanges.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity.Infrastructure;
+using TestLib.Entities;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class EfSavingChanges
+    {
+[TestMethod]
+public void TestSavingChanges()
+{
+    using (var ctx = new CarsContext())
+    {
+        var objCtx = ((IObjectContextAdapter)ctx).ObjectContext;
+
+        bool eventCalled = false;
+
+        objCtx.SavingChanges += (sender, args) => eventCalled = true;
+
+        ctx.SaveChanges();
+
+        Assert.IsTrue(eventCalled);
+    }
+}
+    }
+}
