07af76f6f805db6fdd3d790ea1ac1b493fb2fedd
Anders Abel <anders@abel.nu>
2014-07-18 11:37:15 +0200

    Adding code for test coverage post.

---
+++ TestLib/TestingCoverage/ValueFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib.TestingCoverage
+{
+    public static class ValueFormatter
+    {
+        public static string Format(int? value)
+        {
+            const int defaultValue = 42;
+
+            if(!value.HasValue)
+            {
+                value = defaultValue;
+            }
+
+            return "The value is " + defaultValue + ".";
+        }
+    }
+}
---
+++ TestLibTests/TestingCoverage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib.TestingCoverage;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class TestingCoverage
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var result = ValueFormatter.Format(null);
+
+            Assert.AreEqual("The value is 42.", result);
+        }
+
+        [TestMethod]
+        public void TestDefaultValue()
+        {
+            var result = ValueFormatter.Format(null);
+
+            Assert.AreEqual("The value is 42.", result);
+        }
+
+        [TestMethod]
+        public void TestGivenValue()
+        {
+            var result = ValueFormatter.Format(7);
+
+            Assert.AreEqual("The value is 7.", result);
+        }
+    }
+}
