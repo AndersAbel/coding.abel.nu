58bc75849b1fd0a17c74481c5c20dc66a0a0eb2b
Anders Abel <anders@abel.nu>
2014-12-04 10:52:09 +0100

    Adding code for code coverage and nullable puzzle.

---
+++ TestLib/NullableCodeCoverage/SomeUtilClass.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib.NullableCodeCoverage
+{
+    using MyNumericType = System.Nullable<IntStruct>;
+
+    public struct IntStruct
+    {
+        public IntStruct(int value)
+        {
+            Value = value;
+        }
+
+        public readonly int Value;
+
+        public static bool operator <(IntStruct v1, IntStruct v2)
+        {
+            return v1.Value < v2.Value;
+        }
+
+        public static bool operator >(IntStruct v1, IntStruct v2)
+        {
+            return v1.Value > v2.Value;
+        }
+    }
+
+    public class SomeUtilClass1
+    {
+        public static string IsTooLate(DateTime? value)
+        {
+            if (value < DateTime.Now)
+            {
+                return "No!";
+            }
+
+            return "Yes :-(";
+        }
+
+        public static string IsSingleDigit(int? value)
+        {
+            if (value < 10)
+            {
+                return "Yes!";
+            }
+
+            return "No!";
+        }
+
+        static readonly IntStruct SingleDigitLimit = new IntStruct(10);
+
+        public static string IsSingleDigit(MyNumericType value)
+        {
+            if (value < SingleDigitLimit)
+            {
+                return "Yes!";
+            }
+
+            return "No!";
+        }
+
+        public class IntClass
+        {
+            public IntClass(int value)
+            {
+                Value = value;
+            }
+
+            public readonly int Value;
+
+            public static bool operator <(IntClass v1, IntClass v2)
+            {
+                return v1.Value < v2.Value;
+            }
+
+            public static bool operator >(IntClass v1, IntClass v2)
+            {
+                return v1.Value > v2.Value;
+            }
+        }
+
+        public static string IsSingleDigit(IntClass value)
+        {
+            if (value < new IntClass(10))
+            {
+                return "Yes!";
+            }
+
+            return "No!";
+        }
+    }
+
+    public class SomeUtilClass2
+    {
+        public static string IsTooLate(DateTime? value)
+        {
+            if (value.Value < DateTime.Now)
+            {
+                return "No!";
+            }
+
+            return "Yes :-(";
+        }
+
+        public struct IntStruct
+        {
+            public IntStruct(int value)
+            {
+                Value = value;
+            }
+
+            public readonly int Value;
+
+            public static bool operator <(IntStruct v1, IntStruct v2)
+            {
+                return v1.Value < v2.Value;
+            }
+
+            public static bool operator >(IntStruct v1, IntStruct v2)
+            {
+                return v1.Value > v2.Value;
+            }
+        }
+
+        public static string IsSingleDigit(IntStruct? value)
+        {
+            if (value.Value < new IntStruct(10))
+            {
+                return "Yes!";
+            }
+
+            return "No!";
+        }
+    }
+}
---
+++ TestLibTests/NullableCodeCoverage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLib.NullableCodeCoverage;
+
+namespace TestLibTests
+{
+    [TestClass]
+    public class NullableCodeCoverage
+    {
+        [TestMethod]
+        public void TestImplementation1()
+        {
+            Assert.AreEqual("No!",
+                SomeUtilClass1.IsTooLate(DateTime.Now.AddDays(-1)));
+            Assert.AreEqual("Yes :-(",
+                SomeUtilClass1.IsTooLate(DateTime.Now.AddDays(1)));
+
+            Assert.AreEqual("Yes!", SomeUtilClass1.IsSingleDigit(9));
+            Assert.AreEqual("No!", SomeUtilClass1.IsSingleDigit(10));
+
+            Assert.AreEqual("Yes!", SomeUtilClass1.IsSingleDigit(
+                new IntStruct(9)));
+            Assert.AreEqual("No!", SomeUtilClass1.IsSingleDigit(
+                new IntStruct(10)));
+
+            Assert.AreEqual("Yes!", SomeUtilClass1.IsSingleDigit(
+                new SomeUtilClass1.IntClass(9)));
+            Assert.AreEqual("No!", SomeUtilClass1.IsSingleDigit(
+                new SomeUtilClass1.IntClass(10)));
+        }
+
+        [TestMethod]
+        public void TestImplementation1_IncludingNull()
+        {
+            TestImplementation1();
+
+            Assert.AreEqual("Yes :-(", SomeUtilClass1.IsTooLate(null));
+            Assert.AreEqual("No!", SomeUtilClass1.IsSingleDigit(
+                (IntStruct?)null));
+        }
+
+        [TestMethod]
+        public void TestImplementation2()
+        {
+            Assert.AreEqual("No!",
+                SomeUtilClass2.IsTooLate(DateTime.Now.AddDays(-1)));
+            Assert.AreEqual("Yes :-(",
+                SomeUtilClass2.IsTooLate(DateTime.Now.AddDays(1)));
+
+            Assert.AreEqual("Yes!", SomeUtilClass2.IsSingleDigit(
+                new SomeUtilClass2.IntStruct(9)));
+            Assert.AreEqual("No!", SomeUtilClass2.IsSingleDigit(
+                new SomeUtilClass2.IntStruct(10)));
+        }
+    }
+}
